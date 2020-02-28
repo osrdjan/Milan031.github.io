ed27f593703361df6f38bfe34842ca2ca4650206
Milan031 <60748641+Milan031@users.noreply.github.com>
2020-02-28 16:13:18 +0100

    ad

--- main.js
+++ main.js
@@ -8,27 +8,27 @@ const navSlide = () => {
     /* Dogadjaji pri kliku na hamburger meni */
     burger.addEventListener('click', () => {
         /* Otvaranje i zatvaranje*/
-        nav.classList.toggle('nav-active');        
+        nav.classList.toggle('nav-active');
         navbar.classList.toggle('navbar-active');
         /* Restartovanje animacije i podesavanje duzine animacije pri otvaranju */
         navLi.forEach((link, index) => {
-            if(link.style.animation) {
+            if (link.style.animation) {
                 link.style.animation = ''
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.1}s`;  
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.1}s`;
             }
         });
         /* Promena pozadine oko ikonice */
         burger.classList.toggle('burger-active');
         /* Transformacija ikonice*/
-        burger.classList.toggle('toggle');       
+        burger.classList.toggle('toggle');
     });
 
     /* Zatvaranje hamburger menija pri kliku na link */
     navLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
 
     function navbarLinkClick() {
-        if(nav.classList.contains("nav-active")) {
+        if (nav.classList.contains("nav-active")) {
             burger.click();
         }
     }
